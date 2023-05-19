82f03f59ab540c05336838702ff2c5ea54d8502d
marcinoles87 <marcinoles87@gmail.com>
2023-05-19 16:45:11 +0200

    start with structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
       <Navbar></Navbar>
-    </div>
+        <Switch>
+          <Route  path='/' exact></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React , {useState} from 'react'
+import React , {useState}from 'react'
+import {Link} from 'react-router-dom'
+
 import './navbar.css'
 
 function Navbar() {
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
-        
+        <Link to="/" className="navbar-logo"> Try</Link>
       </div>
     </nav>
     
