b0ac0590e26c2f9807b8fb933d6919fcc5bbc585
marcinoles87 <marcinoles87@gmail.com>
2023-05-19 16:02:11 +0200

    start with structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react'
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Main></Main>
     </div>
   );
 }
