d1f7887fd623ff226b911a3476a215bcc5309b1c
marcinoles87 <marcinoles87@gmail.com>
2023-05-20 08:39:11 +0200

    add menu icon and function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,22 @@ import {Link} from 'react-router-dom'
 import './navbar.css'
 
 function Navbar() {
+  const [click , setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
   return (
     <nav className='navbar'>
-      <div className='navbar-logo'><i className='fab fa-typo3'/></div>
+
+      <div className='navbar-logo'>
+        <i className='fab fa-typo3'/>
+      </div>
+
+      <div className='navbar-menu' onClick={handleClick}>
+        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+      </div>
+
       <div className='navbar-container'>
         <Link to="/" className="navbar-item"> Home</Link>
         <Link to="/" className="navbar-item"> Trips</Link>
