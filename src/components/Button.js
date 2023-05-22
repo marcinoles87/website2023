03187b3b5d47b30a89dc55d6e7b879a06da78c3e
marcinoles87 <marcinoles87@gmail.com>
2023-05-22 21:01:59 +0200

    add components

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,18 @@ export const  Button = ({
     buttonStyle,
     buttonSize
 }) => {
+
     const checkButtonStyle = STYLES.includes(buttonStyle)
      ? buttonStyle 
-     : STYLES[0]
+     : STYLES[0];
+
+     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+     return(
+        <Link to='/sign-up' className="btn-mobile">
+            <button>
+                {children}
+            </button>
+        </Link>
+     )
 };
