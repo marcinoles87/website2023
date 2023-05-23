a96f1c1e9589ce4b498adc775cbee12acaf673af
marcinoles87 <marcinoles87@gmail.com>
2023-05-23 13:40:04 +0200

    add components

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './trips.css'
 
  function Trips() {
   return (
     <div className='trips-container'>
         <div className='trips-list'>
             <div className='trips-card'>
-                <img src='#'></img>
+                
             </div>
         </div>
 
