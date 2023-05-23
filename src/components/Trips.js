b5141e439562cc4069b02976c17cfbe1357371c0
marcinoles87 <marcinoles87@gmail.com>
2023-05-23 19:24:34 +0200

    add components

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './trips.css'
+import data from '../data.json'
 
  function Trips() {
   return (
     <div className='trips-container'>
         <div className='trips-list'>
             <div className='trips-card'>
-                
+                {data.map( (item,index) => {
+                  return(
+                    <div >
+                      <li key={index}>
+                        <h1>{item.name}</h1>
+                        <h2>{item.trip}</h2>
+                      </li>
+                      </div>
+                  )
+                })}
             </div>
         </div>
 
