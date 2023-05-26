edf4f52ec184c3b400d29493595f5e30634fdea7
marcinoles87 <marcinoles87@gmail.com>
2023-05-26 16:04:23 +0200

    add home page

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -7,6 +7,7 @@ import data from '../data.json'
   
   return (
     <div className='trips-container'>
+      <h1>Check out our Trip and Smile</h1>
         <div className='trips-list'>
             <div className='trips-card'>
                 {data.map( (item,index) => {
@@ -16,7 +17,7 @@ import data from '../data.json'
                   return(
                     <div className='trip-item' key={index}>
                       <li key={index} className='trip-item-one'>
-                        <h2 className='trip-name'>{tripName}</h2>
+                        {/* <h2 className='trip-name'>{tripName}</h2> */}
                         <img src={item.im} alt={item.name} className='trip-img'></img>
                         <p className='trip-info'>{item.info}</p>
                         <button className='trip-btn'>More</button>
