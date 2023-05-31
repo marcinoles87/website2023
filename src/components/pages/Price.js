e89dc8ed9bf0d6a07eba93254bd37a8f16f229a4
marcinoles87 <marcinoles87@gmail.com>
2023-05-31 18:49:30 +0200

    add price page

--- src/components/pages/Price.js
+++ src/components/pages/Price.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './price.css'
 
 function Price() {
   return (
-    <div>Price</div>
+    <div className='price-container'>
+      
+
+
+        <div className='price-person'>
+          <h1>Price per person</h1>
+        </div>
+
+        <div className='price-group'>
+          <h1>Price group</h1>
+          </div>
+
+          <div className='price-individual'>
+            <h1>Price individual trip</h1>
+          </div>
+      </div>
   )
 }
 
