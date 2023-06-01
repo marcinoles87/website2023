0408eb213b94f69733e4c6c17ec4a35c435b67fa
marcinoles87 <marcinoles87@gmail.com>
2023-06-01 05:50:46 +0200

    add price page

--- src/components/pages/Price.js
+++ src/components/pages/Price.js
@@ -12,20 +12,36 @@ function Price() {
             <ul className='price-list'>Per Persone
 
                 <li className='price-list-item'>individual site</li>
-                <li className='price-list-item'>individual site</li>
-                <li className='price-list-item'>individual site</li>
-                <li className='price-list-item'>individual site</li>
-                <span className='price-btn'>100E</span>
+                <li className='price-list-item'>personal driver</li>
+                <li className='price-list-item'>best price</li>
+                <li className='price-list-item'>a lot of fun</li>
+                <button className='price-btn'>100E</button>
            </ul>
           
         </div>
 
         <div className='price-group'>
           <h1>Price group</h1>
+          <ul className='price-list'>Per Persone
+
+                <li className='price-list-item'>individual site</li>
+                <li className='price-list-item'>personal driver</li>
+                <li className='price-list-item'>best price</li>
+                <li className='price-list-item'>a lot of fun</li>
+                <button className='price-btn'>100E</button>
+           </ul>
           </div>
 
           <div className='price-individual'>
             <h1>Price individual trip</h1>
+            <ul className='price-list'>Per Persone
+
+                <li className='price-list-item'>individual site</li>
+                <li className='price-list-item'>personal driver</li>
+                <li className='price-list-item'>best price</li>
+                <li className='price-list-item'>a lot of fun</li>
+                <button className='price-btn'>100E</button>
+           </ul>
           </div>
       </div>
   )
