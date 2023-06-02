4c23f6d89277f5239b013e7c31f3ab7d02913f64
marcinoles87 <marcinoles87@gmail.com>
2023-06-02 16:25:54 +0200

    add header plus box

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,7 +68,9 @@ function Header() {
         </div>
 
         <div className='header-information2'>
+          <div className='header-information2-back'>
           <h1> Important Information</h1>
+          </div>
           <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
         </div>
     </div>
