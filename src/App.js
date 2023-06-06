f54fb8a4a891158054a7c831cc15c36cc01ec9f4
marcinoles87 <marcinoles87@gmail.com>
2023-06-06 21:05:55 +0200

    add img function

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
     <Router>
       <Routes>
       
-         <Route exact path='home' element={<Navbar></Navbar>} >
-         <Route  path='home' element={<Home></Home>} ></Route>
-         <Route  path='trips' element={<Trips></Trips>} ></Route>
-         <Route  path='trip' element={<Trip></Trip>} ></Route>
-         <Route  path='price' element={<Price></Price>} ></Route>
-         <Route  path='contact' element={<Contact></Contact>} ></Route>
+         <Route exact path='/' element={<Navbar></Navbar>} >
+         <Route exact path='home' element={<Home></Home>} ></Route>
+         <Route exact path='trips' element={<Trips></Trips>} ></Route>
+         <Route exact  path='trip' element={<Trip></Trip>} ></Route>
+         <Route exact path='price' element={<Price></Price>} ></Route>
+         <Route exact path='contact' element={<Contact></Contact>} ></Route>
          
          </Route>
          
