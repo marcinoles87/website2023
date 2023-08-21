9c0b8424db300cbe5ecc28b39295426206ba32b8
marcinoles87 <marcinoles87@gmail.com>
2023-08-21 23:27:08 +0200

    work with gallery section

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import im1 from '/web frontend/projekty React/website2023/src/img/cracow.jpg'
+
 import './gallery.css'
 
 function Gallery() {
   return (
-    <div className='gallery-container'>Gallery</div>
+    <div className='gallery-container'>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    <img src={im1}></img>
+    
+    </div>
   )
 }
 
