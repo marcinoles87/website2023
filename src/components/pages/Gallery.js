ef145daf8cfbc294897550b26d5d51c4cca1c2e7
marcinoles87 <marcinoles87@gmail.com>
2023-08-22 15:34:30 +0200

    work with gallery section

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import im1 from '/web frontend/projekty React/website2023/src/img/cracow.jpg'
+import im2 from '/web frontend/projekty React/website2023/src/img/hero.jpg'
+import im3 from '/web frontend/projekty React/website2023/src/img/vip1.jpg'
+import im4 from '/web frontend/projekty React/website2023/src/img/vip2.jpg'
 
 import './gallery.css'
 
 function Gallery() {
   return (
     <div className='gallery-container'>
-      <div className='gallery-grid'>
-              <img src={im1}></img>
+      <div className='gallery-row'>
+              <img src={im1} alt='cracow'></img>
+             
+      </div>
+      <div className='gallery-column'>
+              <img src={im3} alt='cracow'></img>
+              <img src={im2} alt='cracow'></img>
+             
+      </div>
+
+      <div className='gallery-column'>
+              <img src={im2} alt='cracow'></img>
+              <img src={im1} alt='cracow'></img>
+             
+      </div>
+
+      <div className='gallery-row'>
+              <img src={im4} alt='cracow'></img>
              
       </div>
     
-    <img src={im1}></img>
-    <img src={im1}></img>
-    <img src={im1}></img>
-    <img src={im1}></img>
-    <img src={im1}></img>
+    <img src={im1} alt='cracow'></img>
+    <img src={im1} alt='cracow'></img>
+    <img src={im1} alt='cracow'></img>
+    <img src={im1} alt='cracow'></img>
+    <img src={im1} alt='cracow'></img>
     
     </div>
   )
